81c6725f2105551d95c804cbb15dc177cf6794a7
qieting <1509134296@qq.com>
2020-01-27 14:05:45 +0800

    初始化了springboot

--- README.md
+++
@@ -1,2 +0,0 @@
-# crowdsourcing
-my graduation design
---
+++ springboot/src/main/java/com/example/crowdsourcing/CrowdsourcingApplication.java
@@ -0,0 +1,13 @@
+package com.example.crowdsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdsourcingApplication.class, args);
+    }
+
+}
